723c61e64733da13a8b651e111e2e37d8d5e3ae2
Dean Hipkiss <dean.hipkiss@gmail.com>
2024-11-07 16:32:50 +0000

    Facebook and insta on heading

--- components/header.js
+++ components/header.js
@@ -14,8 +14,13 @@ class Header extends HTMLElement {
                                 <img src="images/Logo-Small.png" alt="Small Logo">
                             </div>
                         </a>
+ 
+
                         <div class="hideMobile">
+
                             <ul class="nav-links">
+                                
+            
                                 <li><a href="#">Home</a></li>
                                 
                                 <li class="dropdown">
@@ -24,6 +29,8 @@ class Header extends HTMLElement {
                                 <ul class="dropdown-menu">
                                         <li>&nbsp;</li>
                                         <li><a href="#TheatreSchool">Theatre School</a></li>
+                                        <li><a href="#brackley"><span>&#8226;</span>&nbsp;Brackley</a></li>
+                                        <li><a href="#warwick"><span>&#8226;</span>&nbsp;Warwick and Leamington Spa</a></li>
                                         <li><a href="#HolidayCamps">Holiday Camps & Workshops</a></li>
                                         <li><a href="#AfterSchool">After School & LunchClubs</a></li>
                                         <li><a href="#PrivateLessons">Private Lessons</a></li>
@@ -34,10 +41,17 @@ class Header extends HTMLElement {
                                 <li><a href="https://app.classmanager.com/classes/ising-idance-iact-theatre-school">Bookings</a></li>
                                 <li><a href="#Testimony">Testimony</a></li>
                                 <li><a href="#Staff">Staff</a></li>
+                                <li><div class="svgLink"><a href="https://www.facebook.com/iSDATheatreSchool/"> <img src="images/square-facebook-brands-solid-white.svg" /> </a></div></li>
+                                <li><div class="svgLink"><a  href="https://www.instagram.com/isingidanceiacttheatreschool/"><img src="images/square-instagram-brands-solid-white.svg" /> </a></div></li>
+
                             </ul>
 
                         </div>
+
                         <div class="hideDesktop">
+
+                            <div class="svgLink"><a href="https://www.facebook.com/iSDATheatreSchool/"> <img src="images/square-facebook-brands-solid-white.svg" /> </a></div>
+                            <div class="svgLink"><a  href="https://www.instagram.com/isingidanceiacttheatreschool/"><img src="images/square-instagram-brands-solid-white.svg" /> </a></div>
                             <div class="hamburger">
                                 <span class="bar"></span>
                                 <span class="bar"></span>
@@ -53,13 +67,15 @@ class Header extends HTMLElement {
 
         <div id="sidenav" class="sidenav">
             <div class="sidebaveTop">
-                <div class="sidenavTitle">iSDA</div>
+                <div class="sidenavTitle headlineFont headingFont1">iSDA</div>
                 <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
             </div>
             
             <a class="sidenavLink" href="#">Home</a>
 
             <a class="sidenavLink" href="#TheatreSchool">Theatre School</a>
+            <a class="sidenavLink" href="#brackley"><span>&#8226;</span>&nbsp;Brackley</a>
+            <a class="sidenavLink" href="#warwick"><span>&#8226;</span>&nbsp;Warwick and Leamington Spa</a>
             <a class="sidenavLink" href="#HolidayCamps">Holiday Camps & Workshops</a>
             <a class="sidenavLink" href="#AfterSchool">After School & LunchClubs</a>
             <a class="sidenavLink" href="#PrivateLessons">Private Lessons</a>
